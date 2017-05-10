9910f99000659075a9dc70a23ab6744e87f77f5c
vanessaelizabeth <vanessaelizabethmero@hotmail.com>
2017-05-09 22:47:09 -0500

    Add files via upload

---
+++ Personal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pregunta4;
+
+/**
+ *
+ * @author 
+ */
+public class Personal extends Empleado {
+
+    public Personal() {
+    }
+    
+    
+}
